35e0655f283db71f9fef73d890500feae3266cb7
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-09-04 18:46:19 +0100

    add navbar add hero sect add featuredservice

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,23 @@ module.exports = {
       colors: {
         "ff-blue": "#011A48",
         "ff-btn-blue": "#2B367A",
-        "ff-yellow": "#DFC99F"
-      }
+        "ff-yellow": "#DFC99F",
+        "ff-grey": "#686868"
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
